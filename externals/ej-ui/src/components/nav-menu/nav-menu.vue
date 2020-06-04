878f753870804356aa326b12cf2eca24198e5bef
cfchen01 <674441755@qq.com>
2020-06-04 18:41:06 +0800

    first commit

---
+++ externals/ej-ui/src/components/nav-menu/nav-menu.vue
@@ -0,0 +1,79 @@
+<script lang="jsx">
+  import {Menu as ElMenu} from 'element-ui'
+
+  export default {
+    name: 'EjNavMenu',
+
+    functional: true,
+
+    render: (h, {data, children}) => {
+      data.staticClass = (data.staticClass || '') + ' ej-nav-menu'
+
+      return <ElMenu {...data}>{children}</ElMenu>
+    },
+  }
+</script>
+
+<style lang="scss">
+  .ej-nav-menu {
+
+    &.el-menu {
+      background: #1F2E4D;
+
+      &.el-menu--horizontal {
+        @apply border-b-0 flex;
+
+        > * {
+          margin-left: 50px;
+          opacity: .65;
+          padding: 0 12px;
+
+          &:first-child {
+            margin-left: 0;
+          }
+
+          &:hover,
+          &.is-opened,
+          &.is-active {
+            opacity: 1;
+          }
+          &.is-active {
+            background: rgba(255, 255, 255, .1) !important;
+          }
+        }
+
+        > .el-menu-item,
+        > .el-submenu .el-submenu__title {
+          @apply p-0 border-b-0;
+
+          height: inherit;
+          font-size: 16px;
+          line-height: inherit;
+
+          &:hover,
+          &:focus {
+            background: transparent;
+          }
+        }
+
+        > .el-menu-item {
+          padding: 0 12px;
+
+          &.is-active {
+            @apply border-0;
+            background: rgba(255, 255, 255, .1) !important;
+          }
+        }
+
+        > .el-submenu {
+          .el-submenu__title {
+          }
+        }
+      }
+    }
+
+    @at-root .ej-app-logo + & {
+      margin-left: 172px;
+    }
+  }
+</style>
---
+++ externals/ej-ui/src/components/nav-menu/nav-submenu.vue
@@ -0,0 +1,65 @@
+<script lang="jsx">
+  import {Submenu as ElSubmenu} from 'element-ui'
+
+  export default {
+    name: 'EjNavSubmenu',
+
+    functional: true,
+
+    render: (h, {data, slots, children}) => {
+      data.staticClass = (data.staticClass || '') + ' ej-nav-submenu'
+      data.attrs['popper-class'] = (data.attrs['popper-class'] || '') + ' ej-nav-submenu-popper'
+
+      return (
+        <ElSubmenu {...{...data, slots}}>{children}</ElSubmenu>
+      )
+    },
+  }
+</script>
+
+<style lang="scss">
+  // .ej-nav-submenu {
+  //   .el-submenu__icon-arrow {
+  //     @apply hidden;
+  //   }
+  // }
+
+  .ej-nav-submenu-popper {
+    .el-menu--popup {
+      background: #1F2E4D;
+
+      .el-menu-item,
+      .el-submenu,
+      .el-submenu__title {
+        background: transparent !important;
+        color: rgba(255, 255, 255, .65) !important;
+
+        &:hover {
+          @apply text-white #{!important};
+        }
+      }
+
+      .el-menu-item,
+      .el-submenu {
+        &:hover {
+
+        }
+
+        &.is-active {
+          @apply text-white #{!important};
+          background: theme('colors.blue.default') !important;
+          > .el-submenu__title {
+            @apply text-white #{!important};
+            background: theme('colors.blue.default') !important;
+          }
+        }
+
+        &.is-opened {
+          > .el-submenu__title {
+            @apply text-white #{!important};
+          }
+        }
+      }
+    }
+  }
+</style>
